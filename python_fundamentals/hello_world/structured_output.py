3224046fbdccd8c507c7c326a0cb9ab08d36cdad
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-09-16 20:35:58 +0300

    Fix syntax structured_output.py

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
-Language = "python"
-Version = 3
+#!/usr/bin/python3
+language = "paython"
+version = 3
 pi_approx = 3.14159
-computation_valid = 2>1
+computation_valid = 2 > 1
 
-print(f"language:{Language}")
-print(f"version:{Version}")
-print(f"pi_approx:{pi_approx:.2f}")
-print(f"coputation_valied:{computation_valid}")
+print(f"Language: {language}")
+print(f"version:{version}")
+print(f"pi approx: {pi_approx:.2f}")
+print(f"computation valid: {computation_valid}")
