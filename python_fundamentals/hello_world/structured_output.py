0d469e78f0d3d6429a33ec4b8397a0fcdfb452ac
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-09-16 20:11:16 +0300

    init

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+Language = "python"
+Version = 3
+pi_approx = 3.14159
+computation_valid = 2>1
+
+print(f"language:{Language}")
+print(f"version:{Version}")
+print(f"pi_approx:{pi_approx:.2f}")
+print(f"coputation_valied:{computation_valid}")
