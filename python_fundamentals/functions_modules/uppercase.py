9251144231df0788147c677d7d24f6474193c44d
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-09-19 06:49:26 +0200

    Add 1-uppercase task

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    result = ""
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print("{}".format(result))
